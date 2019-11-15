9b9e6c7dc05a97cb7b86f65abb2d4b3832eb48d9
3daddict <print@3daddict.com>
2019-11-14 22:37:53 -0800

    working with pagination

--- products/product_variant.js
+++ products/product_variant.js
@@ -1,6 +1,7 @@
 const path = require('path')
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
 const Shopify = require('../node_modules/shopify-api-node');
+const writeJSON = require('../components/writeJSON');
  
 const shopify = new Shopify({
   shopName: process.env.SHOPNAME,
@@ -9,13 +10,14 @@ const shopify = new Shopify({
 });
 
 (async () => {
-    let params = { limit: 10 };
+    let params = { limit: 50 };
    
     do {
       const products = await shopify.product.list(params);
    
       console.log(products);
-   
+
       params = products.nextPageParameters;
+      writeJSON(products, './data/product_variants.json')
     } while (params !== undefined);
   })().catch(console.error);
